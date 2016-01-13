f74aa1a6466180f222f9934cfda898f002e604c5
ToukaiAkihiro <tokaimas@yahoo.co.jp>
2016-01-13 22:35:24 +0900

    Intital commit

---
+++ js/custom.js
@@ -0,0 +1,88 @@
+﻿/*=============================================================
+    Authour URL: www.designbootstrap.com
+    
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+
+ /*====================================
+          SUBSCRIPTION   SCRIPTS 
+    ======================================*/
+
+
+   $("#postcontent").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "subscribe.php",
+            data: $("#postcontent").serialize(),
+             success: function (response) {
+			  $('[name="email"]').val('');
+               // alert(response); // FOR ACTUAL RESPONSE
+			   alert('Thanks for  subscribing Us');
+            }
+        });
+        e.preventDefault();
+    });
+ 
+   /*====================================
+           COUNTDOWN CUSTOM SCRIPTS 
+   ======================================*/
+
+    setInterval(function () {
+
+        var enddate = new Date("Jan 24 2016 06:30:00 GMT+09:00"); // change your date here like Jan 10 2016 GMT+05:00
+
+        var today = new Date();
+
+        var difference = Math.floor((enddate.getTime() - today.getTime()) / 1000);
+
+        var seconds = GlobalFunctn(difference % 60);
+
+        difference = Math.floor(difference / 60);
+
+        var minutes = GlobalFunctn(difference % 60);
+
+        difference = Math.floor(difference / 60);
+
+        var hours = GlobalFunctn(difference % 24);
+
+        difference = Math.floor(difference / 24);
+
+        var days = difference;
+
+        $("#second-number").text(seconds);
+
+        $("#minute-number").text(minutes);
+
+        $("#hour-number").text(hours);
+
+        $("#day-number").text(days);
+
+    }, 1000);
+
+    function GlobalFunctn(valuesmy) {
+
+        if (valuesmy < 0)
+
+            valuesmy = 0;
+
+        if (valuesmy < 10)
+
+            return "0" + valuesmy;
+
+        return "" + valuesmy;
+
+    }
+});
+
---
+++ js/replenish.js
@@ -0,0 +1,49 @@
+$(function(){
+    var setElm = $('.split'),
+    delaySpeed = 550,
+    fadeSpeed = 100;
+ 
+    setText = setElm.html();
+ 
+    setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
+        var elmThis = $(this);
+        if (this.nodeType == 3) {
+            var $this = $(this);
+            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+        }
+    });
+    $(window).load(function(){
+        splitLength = $('.textSplitLoad').length;
+        setElm.find('.textSplitLoad').each(function(i){
+            splitThis = $(this);
+            splitTxt = splitThis.text();
+            splitThis.delay(i*(delaySpeed)).css({display:'inline-block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+        });
+        setTimeout(function(){
+                setElm.html(setText);
+        },splitLength*delaySpeed+fadeSpeed);
+    });
+});
+
+function setSize() {
+    //画像サイズ指定
+    var imgW = 2000;
+    var imgH = 1332;
+    //ウィンドウサイズ取得
+    var winW = $(window).width();
+    var winH = $(window).height();
+    var scaleW = winW / imgW;
+    var scaleH = winH / imgH;
+    var fixScale = Math.max(scaleW, scaleH);
+    var setW = imgW * fixScale;
+    var setH = imgH * fixScale;
+    var moveX = Math.floor((winW - setW) / 2);
+    var moveY = Math.floor((winH - setH) / 2);
+ 
+    $('#section0').css({
+        'width': setW,
+        'height': setH,
+        'left' : moveX,
+        'top' : moveY
+    });
+}
